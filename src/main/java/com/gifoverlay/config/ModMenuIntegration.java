0de7957a7b2199c9f9c00579dbe0151603db3d5e
max8086243-cpu <max8086243@gmail.com>
2026-05-08 21:38:55 +0300

    Update ModMenuIntegration.java

--- src/main/java/com/gifoverlay/config/ModMenuIntegration.java
+++ src/main/java/com/gifoverlay/config/ModMenuIntegration.java
@@ -3,13 +3,10 @@ package com.gifoverlay.config;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import com.gifoverlay.gui.GifSettingsScreen;
-import net.fabricmc.api.Environment;
-import net.fabricmc.api.EnvType;
 
-@Environment(EnvType.CLIENT)
 public class ModMenuIntegration implements ModMenuApi {
-@Override
-public ConfigScreenFactory<?> getModConfigScreenFactory() {
-return parent -> new GifSettingsScreen(parent);
-}
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> new GifSettingsScreen(parent);
+    }
 }
