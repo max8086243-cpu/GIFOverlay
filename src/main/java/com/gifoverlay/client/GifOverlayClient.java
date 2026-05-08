65cf864f205f012658ab9b1954b2ae3b57d31eb8
max8086243-cpu <max8086243@gmail.com>
2026-05-08 21:50:45 +0300

    Update GifOverlayClient.java

--- src/main/java/com/gifoverlay/client/GifOverlayClient.java
+++ src/main/java/com/gifoverlay/client/GifOverlayClient.java
@@ -1,5 +1,6 @@
 package com.gifoverlay.client;
 
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import com.gifoverlay.config.ModConfig;
